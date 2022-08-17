622db993baebc9cc11e1e68fc1f34e38be3d176a
[firstname lastname] <[valid-email]>
2022-08-17 16:02:02 +0530

    javascript if else

---
+++ ifelse.js
@@ -0,0 +1,13 @@
+var x = 500;
+if (x<=10){
+console.log('javascript a');
+}
+else if (x>10 && x<=50){
+    console.log('you got book b');
+}
+else if(x>50 && x<=122){
+    console.log('you got pen c');
+}
+else{
+    console.log('you got nothing');
+}
